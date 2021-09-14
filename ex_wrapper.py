19f4b7327d437e27caab3466def1a7a6552a931a
balduser <36861693+balduser@users.noreply.github.com>
2021-09-14 16:22:32 +0700

    Update ex_wrapper.py

--- ex_wrapper.py
+++ ex_wrapper.py
@@ -1,8 +1,23 @@
 from Exscript import Account
 from Exscript.protocols import SSH2
-# To be done: if-range, description, interfaces
+# To be done: description, execute_file
 class Commutator:
-    """Creates objects that may be interacted like commutators"""
+    """Creates objects that may be interacted like commutators
+    Method performed with a commutator (commutator-methods) are:
+        execute_file()
+        show_int()
+        show_run()
+        show_sw()
+        version()
+    Methods performed with port (port-methods) are:
+        get_poe(port, **kwargs)
+        get_vlan(port, **kwargs)
+        link_state(port)
+        set_poe(port, **kwargs)
+        set_vlan(port, **kwargs)
+        show_int(port)
+        show_run(port)
+    Port-methods always get the port number as a first argument (see parse_portnumber() docstring)."""
 
     default_login = 'admin'
     default_password = 'admin'
@@ -25,27 +40,99 @@ class Commutator:
         self.__conn.execute('terminal datadump')
         print('Logged in')
 
+
     def __repr__(self):
         return f'{self.brand} commutator, ip {self.ip}'
 
-    def show_run(self, *args: int, **kwargs):
-        """With no arguments returns result of `show running-config` command
-        To get the result `show running-config interface <>` it takes to give an integer argument
+
+    def parse_portnumber(self, *args):
+        """Parsing port from the first position argument
+        as int:
             1 for GigabitEthernet 1/0/1,
             24 for GigabitEthernet 1/0/24,
-            101 for TengigabitEthernet 1/0/1, etc.
-        or interface=['gi',1,0,1] for GigabitEthernet 1/0/1,
-            interface=['te',1,0,4] for TengigabitEthernet 1/0/4
+            222 for GigabitEthernet 2/0/22,
+            1001 for TengigabitEthernet 1/0/1,
+            1404 for TengigabitEthernet 4/0/4 etc
+        as list:
+            ['gi',1,0,1] for GigabitEthernet 1/0/1,
+        as string:
+            "po 1" for port-channel 1
+            "whatever" for whatever"""
+
+        interface = args[0]
+        if isinstance(interface, int):
+                if interface < 100:
+                    return f'gi 1/0/{interface}'
+                elif 100 < interface < 1000:
+                    return f'gi {interface // 100}/0/{interface % 100}'
+                elif 1000 < interface < 1100:
+                    return f'te 1/0/{interface%100}'
+                else:
+                    return f'te {(interface-1000) // 100}/0/{interface % 100}'
+        elif isinstance(interface, list):
+                return f'{interface[0]} {interface[1]}/{interface[2]}/{interface[3]}'
+        elif isinstance(interface, str):
+                return interface
+
+
+    def show_int(self, *args):
+        """With no arguments returns result of `show interfaces` command
+        To get the result `show interfaces <>` it takes the port number argument
         Returns a string"""
 
-        if args or kwargs:
-            interface = self.parse_portnumber(*args, **kwargs)
+        if args:
+            interface = self.parse_portnumber(*args)
+            command = f'sh int {interface}'
+        else:
+            command = 'sh int'
+        self.__conn.execute(command)
+        answer = self.__conn.response
+        return answer
+
+
+    def show_run(self, *args):
+        """With no arguments returns result of `show running-config` command
+        To get the result `show running-config interface <>` it takes the port number argument
+        Returns a string"""
+
+        if args:
+            interface = self.parse_portnumber(*args)
             command = f'sh ru int {interface}'
         else:
             command = 'sh ru'
         self.__conn.execute(command)
         answer = self.__conn.response
         return answer
+
+
+    def show_sw(self):
+
+        try:
+            self.__conn.execute('show version')
+            answer = self.__conn.response
+            for line in answer.split('\n'):
+                if 'Version' in line:
+                    return line.split('Version:')[1].strip()
+        except:
+            pass
+
+    def link_state(self, *args):
+            """Returns 1 is port's link state is up and 0 if it's down"""
+
+        try:
+            interface = self.parse_portnumber(*args)
+            self.__conn.execute(f'show int status {interface}')
+            answer = self.__conn.response.split('\n')
+            for i, line in enumerate(answer):
+                if 'Port' in line:
+                    ans = answer[i+2].split()[6]
+                    if ans == 'Up':
+                        return True
+                    elif ans == 'Down':
+                        return False
+        except:
+            pass
+
 
     def get_vlan(self, *args, **kwargs):
         """Returns information about vlans setup. Cannot be launched without arguments, selecting an interface is necessary.
@@ -57,7 +144,7 @@ class Commutator:
         With a parameter mode='mode' returns a string:
             'access' | 'trunk' | 'customer' | 'general' """
 
-        answer = self.show_run(*args, **kwargs)
+        answer = self.show_run(*args)
         if 'mode' in kwargs:
             if kwargs['mode'] == 'mode':
                 for line in answer.split('\n'):
@@ -96,7 +183,7 @@ class Commutator:
             {'mode': 'access', 'access': 107, 'mtv': 118}
         Empty dict {} means removing all switchport records"""
 
-        interface = self.parse_portnumber(*args, **kwargs)
+        interface = self.parse_portnumber(*args)
         for arg in args:
             if isinstance(arg, dict):
                 current_setup = self.get_vlan(*args, **kwargs)
@@ -164,10 +251,10 @@ class Commutator:
         returns False is operational Poe status is off or fault
         if mode='current', returns float (mA)
         if mode='voltage', returns float (Volts)
-        if mode='details', returns detailed info about the port
+        if mode='verbose', returns detailed info about the port
         in case of trouble returns None"""
 
-        interface = self.parse_portnumber(*args, **kwargs)
+        interface = self.parse_portnumber(*args)
         # Somewhy Exscript throws InvalidCommandException after requesting Poe. But output is ok
         try:
             command = f'show power inline {interface}'
@@ -179,7 +266,7 @@ class Commutator:
                 pass
         try:
             if 'mode' in kwargs:
-                if kwargs['mode'] == 'details':
+                if kwargs['mode'] == 'verbose':
                     return answer
                 if kwargs['mode'] == 'current':
                     for line in answer.split('\n'):
@@ -199,12 +286,13 @@ class Commutator:
         except:
             pass
 
+
     def set_poe(self, *args, **kwargs):
         """Setting POE operational status on a port
-        Gets a portnumber and True/False. For True set power inline to 'auto', for False - to 'never'
+        Gets a portnumber and True/False. For True sets power inline to 'auto', for False - to 'never'
         After the succesfull execution returns True, in case of trouble returns None"""
 
-        interface = self.parse_portnumber(*args, **kwargs)
+        interface = self.parse_portnumber(*args)
         try:
             self.__conn.execute('config')
             self.__conn.execute(f'interface {interface}')
@@ -217,30 +305,6 @@ class Commutator:
             return True
         except:
             pass
-
-    def parse_portnumber(self, *args, **kwargs):
-        """Parsing port from the first integer position argument or from the keyword argument 'interface: int'
-            1 for GigabitEthernet 1/0/1,
-            24 for GigabitEthernet 1/0/24,
-            222 for GigabitEthernet 2/0/22,
-            1001 for TengigabitEthernet 1/0/1,
-            1404 for TengigabitEthernet 4/0/4 etc.
-        or interface=['gi',1,0,1] for GigabitEthernet 1/0/1,
-            interface=['te',1,0,4] for TengigabitEthernet 1/0/4"""
-
-        if args:
-            interface = [arg for arg in args if isinstance(arg, int)][0]
-            if interface < 100:
-                return f'gi 1/0/{interface}'
-            elif 100 < interface < 1000:
-                return f'gi {interface // 100}/0/{interface % 100}'
-            elif 1000 < interface < 1100:
-                return f'te 1/0/{interface%100}'
-            else:
-                return f'te {(interface-1000) // 100}/0/{interface % 100}'
-        elif 'interface' in kwargs:
-            interface = kwargs['interface']
-            return f'{interface[0]} {interface[1]}/{interface[2]}/{interface[3]}'
 
 
     def __del__(self):
